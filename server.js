7405ea168d45d5b392453d39bb2e44b9b9fa8fa8
Rajesh1951 <rajesh2oo1code@gmail.com>
2022-10-18 22:33:31 +0530

    responsive background images

--- server.js
+++ server.js
@@ -31,8 +31,28 @@ app.post('/postsubmit', (req, res) => {
                 var setTime = new Date(jsonObj["sys"]["sunset"] * 1000);
                 let setTimeString = setTime.toLocaleTimeString();
                 let weather = jsonObj["weather"][0]["main"];
+                let bgroundlink;
+                if (weather == "Clouds") {
+                    bgroundlink = 'https://live.staticflickr.com/3087/2545618443_72d516c651_c.jpg';
+                }
+                else if (weather == 'Clear') {
+                    bgroundlink = 'https://clarksvillenow.sagacom.com/files/2020/10/shutterstock_206307496-1200x768.jpg';
+                }
+                else if (weather == 'Fog') {
+                    bgroundlink = 'https://media.autoweek.nl/m/e6oyzqfbxk5o_800.jpg';
+                }
+                else if (weather == 'Rain') {
+                    bgroundlink = 'https://previews.123rf.com/images/pinkomelet/pinkomelet1607/pinkomelet160700194/60305231-blue-drops-falling-down-of-heavy-rain-weather-on-water.jpg';
+                }
+                else if (weather == 'Mist') {
+                    bgroundlink = 'https://images.hdqwalls.com/wallpapers/field-fog-trees-blue-weather-cold-4k-5k-ee.jpg';
+                }
+                else if (weather == 'Haze') {
+                    bgroundlink = 'https://brightpunjabexpress.com/wp-content/uploads/2020/11/foggy-weather.png';
+                }
                 temperature.toString();
                 res.render('index2', {
+                    background_link: bgroundlink,
                     city: city_input.toLocaleUpperCase(),
                     temp: temperature,
                     tmax: maxTemp,
@@ -61,8 +81,28 @@ app.get('/link/:city', (req, res) => {
             var setTime = new Date(jsonObj["sys"]["sunset"] * 1000);
             let setTimeString = setTime.toLocaleTimeString();
             let weather = jsonObj["weather"][0]["main"];
+            let bgroundlink;
+            if (weather == "Clouds") {
+                bgroundlink = 'https://live.staticflickr.com/3087/2545618443_72d516c651_c.jpg';
+            }
+            else if (weather == 'Clear') {
+                bgroundlink = 'https://clarksvillenow.sagacom.com/files/2020/10/shutterstock_206307496-1200x768.jpg';
+            }
+            else if (weather == 'Fog') {
+                bgroundlink = 'https://media.autoweek.nl/m/e6oyzqfbxk5o_800.jpg';
+            }
+            else if (weather == 'Rain') {
+                bgroundlink = 'https://previews.123rf.com/images/pinkomelet/pinkomelet1607/pinkomelet160700194/60305231-blue-drops-falling-down-of-heavy-rain-weather-on-water.jpg';
+            }
+            else if (weather == 'Mist') {
+                bgroundlink = 'https://images.hdqwalls.com/wallpapers/field-fog-trees-blue-weather-cold-4k-5k-ee.jpg';
+            }
+            else if (weather == 'Haze') {
+                bgroundlink = 'https://brightpunjabexpress.com/wp-content/uploads/2020/11/foggy-weather.png';
+            }
             temperature.toString();
             res.render('index2', {
+                background_link: bgroundlink,
                 city: city_input.toLocaleUpperCase(),
                 temp: temperature,
                 tmax: maxTemp,
